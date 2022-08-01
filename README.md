efd775133b35893bcddf900671484b15475d2696
Nadun De Silva <nadunrds@gmail.com>
2022-08-01 23:03:45 +0530

    Add base project

---
+++ README.md
@@ -0,0 +1,3 @@
+## Sample Pet Store [WIP]
+
+This is a sample application of a Pet Store.
---
+++ services/customers/src/test/java/io/github/nadundesilva/samples/petstore/customers/CustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nadundesilva.samples.petstore.customers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
