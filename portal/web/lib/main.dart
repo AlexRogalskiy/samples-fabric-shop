b055e05709e9921bb2dff1def5fae957d11a30b7
Nadun De Silva <nadunrds@gmail.com>
2023-02-19 17:17:00 +1300

    Add VSCode configs for flutter

--- portal/web/lib/main.dart
+++ portal/web/lib/main.dart
@@ -96,11 +96,11 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'You have clicked the button this many times:',
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
